be1f21bb6e1dd83d203d9da3c5b365f4bd406266
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-04-16 11:18:03 +0330

    Update the pathes

--- DFTND/dataset_wrapper.py
+++ DFTND/dataset_wrapper.py
@@ -6,8 +6,8 @@ import seaborn as sns
 from scipy import stats
 from tqdm import tqdm, tqdm_notebook
 import matplotlib.pyplot as plt
-from robustness import model_utils, datasets
-from robustness.tools.vis_tools import show_image_row, show_image_column
+from robustrobustness_lib.robustnessness import model_utils, datasets
+from robustness_lib.robustness.tools.vis_tools import show_image_row, show_image_column
 #from robustness.tools.constants import CLASS_DICT
 #from user_constants import DATA_PATH_DICT
 import math
